3d52e95245a6f3db1934cf80e9f820cce6e83f78
Alex Mir <expressprogress@ya.ru>
2021-06-08 20:39:23 +0300

    feat: currentUser context scope

--- src/components/App.js
+++ src/components/App.js
@@ -1,82 +1,96 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../index.css'
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
 import PopupWithForm from './PopupWithForm'
 import ImagePopup from './ImagePopup'
+import { api } from '../utils/api'
+import { CurrentUserContext } from '../contexts/CurrentUserContext'
 
 const initialPopupState = { isEditAvatarPopupOpen: false, isEditProfilePopupOpen: false, isAddPlacePopupOpen: false, isOverviewPopupOpen: false }
 
 function App() {
   const [popupState, setPopupState] = useState(initialPopupState)
   const [selectedCard, setSelectedCard] = useState(null)
+  const [currentUser, setCurrentUser] = useState(null)
+  useEffect(() => {
+    api
+      .getUserData()
+      .then(data => {
+        setCurrentUser(data)
+      })
+      .catch(err => {
+        console.log(err + ' && Ошибка при получении данных пользователя')
+      })
+  }, [])
   const handleCardClick = cardData => {
     setSelectedCard(cardData)
   }
-
   const closeAllPopups = () => {
     setPopupState(initialPopupState)
     setSelectedCard(null)
   }
   return (
-    <div>
-      <Header />
-      <Main
-        onEditAvatar={() => {
-          setPopupState({ ...popupState, isEditAvatarPopupOpen: true })
-        }}
-        onEditProfile={() => {
-          setPopupState({ ...popupState, isEditProfilePopupOpen: true })
-        }}
-        onAddPlace={() => {
-          setPopupState({ ...popupState, isAddPlacePopupOpen: true })
-        }}
-        onOverviewPopup={() => {
-          setPopupState({ ...popupState, isOverviewPopupOpen: true })
-        }}
-        onCardClick={handleCardClick}
-      />
-      <Footer />
-      <PopupWithForm title='Редактировать профиль' name='profile-editor' isOpen={popupState.isEditProfilePopupOpen} onClose={closeAllPopups} submitBtnText='Сохранить'>
-        <div className='popup__input-wrapper'>
-          <input className='popup__input popup__input_data_name' id='profile-name' name='name' placeholder='Имя' type='text' minLength='2' maxLength='40' autoComplete='off' required />
-          <span className='popup__input-error' id='profile-name-error'>
-            Вы пропустили это поле.
-          </span>
-        </div>
-        <div className='popup__input-wrapper'>
-          <input className='popup__input popup__input_data_description' id='profile-job' name='description' placeholder='Вид деятельности' type='text' minLength='2' maxLength='200' autoComplete='off' required />
-          <span className='popup__input-error' id='profile-job-error'>
-            Вы пропустили это поле.
-          </span>
-        </div>
-      </PopupWithForm>
-      <PopupWithForm title='Новое место' name='card-renderer' isOpen={popupState.isAddPlacePopupOpen} onClose={closeAllPopups} submitBtnText='Сохранить'>
-        <div className='popup__input-wrapper'>
-          <input className='popup__input popup__input_data_name' placeholder='Название' id='pic-name' name='name' type='text' minLength='2' maxLength='30' autoComplete='off' required />
-          <span className='popup__input-error' id='pic-name-error'>
-            Вы пропустили это поле.
-          </span>
-        </div>
-        <div className='popup__input-wrapper'>
-          <input className='popup__input popup__input_data_description' placeholder='Ссылка на картинку' id='url' type='url' name='url' autoComplete='on' required />
-          <span className='popup__input-error popup__input-error_description' id='url-error'>
-            Введите адрес сайта.
-          </span>
-        </div>
-      </PopupWithForm>
-      <PopupWithForm title='Обновить аватар' name='avatar-updater' isOpen={popupState.isEditAvatarPopupOpen} onClose={closeAllPopups} submitBtnText='Сохранить'>
-        <div className='popup__input-wrapper'>
-          <input id='avatar-upd-input' className='popup__input popup__input_avatar popup__input_data_description' placeholder='Ссылка на картинку' type='url' name='url' autoComplete='on' required />
-          <span id='avatar-upd-input-error' className='popup__input-error popup__input-error_description popup__input-error_avatar'>
-            Введите адрес картинки.
-          </span>
-        </div>
-      </PopupWithForm>
-      <PopupWithForm title='Вы уверены?' name='delete-card' />
-      <ImagePopup card={selectedCard} isOpen={popupState.isOverviewPopupOpen} onClose={closeAllPopups} />
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div>
+        <Header />
+        <Main
+          onEditAvatar={() => {
+            setPopupState({ ...popupState, isEditAvatarPopupOpen: true })
+          }}
+          onEditProfile={() => {
+            setPopupState({ ...popupState, isEditProfilePopupOpen: true })
+          }}
+          onAddPlace={() => {
+            setPopupState({ ...popupState, isAddPlacePopupOpen: true })
+          }}
+          onOverviewPopup={() => {
+            setPopupState({ ...popupState, isOverviewPopupOpen: true })
+          }}
+          onCardClick={handleCardClick}
+        />
+        <Footer />
+        <PopupWithForm title='Редактировать профиль' name='profile-editor' isOpen={popupState.isEditProfilePopupOpen} onClose={closeAllPopups} submitBtnText='Сохранить'>
+          <div className='popup__input-wrapper'>
+            <input className='popup__input popup__input_data_name' id='profile-name' name='name' placeholder='Имя' type='text' minLength='2' maxLength='40' autoComplete='off' required />
+            <span className='popup__input-error' id='profile-name-error'>
+              Вы пропустили это поле.
+            </span>
+          </div>
+          <div className='popup__input-wrapper'>
+            <input className='popup__input popup__input_data_description' id='profile-job' name='description' placeholder='Вид деятельности' type='text' minLength='2' maxLength='200' autoComplete='off' required />
+            <span className='popup__input-error' id='profile-job-error'>
+              Вы пропустили это поле.
+            </span>
+          </div>
+        </PopupWithForm>
+        <PopupWithForm title='Новое место' name='card-renderer' isOpen={popupState.isAddPlacePopupOpen} onClose={closeAllPopups} submitBtnText='Сохранить'>
+          <div className='popup__input-wrapper'>
+            <input className='popup__input popup__input_data_name' placeholder='Название' id='pic-name' name='name' type='text' minLength='2' maxLength='30' autoComplete='off' required />
+            <span className='popup__input-error' id='pic-name-error'>
+              Вы пропустили это поле.
+            </span>
+          </div>
+          <div className='popup__input-wrapper'>
+            <input className='popup__input popup__input_data_description' placeholder='Ссылка на картинку' id='url' type='url' name='url' autoComplete='on' required />
+            <span className='popup__input-error popup__input-error_description' id='url-error'>
+              Введите адрес сайта.
+            </span>
+          </div>
+        </PopupWithForm>
+        <PopupWithForm title='Обновить аватар' name='avatar-updater' isOpen={popupState.isEditAvatarPopupOpen} onClose={closeAllPopups} submitBtnText='Сохранить'>
+          <div className='popup__input-wrapper'>
+            <input id='avatar-upd-input' className='popup__input popup__input_avatar popup__input_data_description' placeholder='Ссылка на картинку' type='url' name='url' autoComplete='on' required />
+            <span id='avatar-upd-input-error' className='popup__input-error popup__input-error_description popup__input-error_avatar'>
+              Введите адрес картинки.
+            </span>
+          </div>
+        </PopupWithForm>
+        <PopupWithForm title='Вы уверены?' name='delete-card' />
+        <ImagePopup card={selectedCard} isOpen={popupState.isOverviewPopupOpen} onClose={closeAllPopups} />
+      </div>
+    </CurrentUserContext.Provider>
   )
 }
 
